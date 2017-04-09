1c3ea350a5920e3741365e6cde4fe29ac2ee70e9
Jay Ullman <jayullman@gmail.com>
2017-04-08 18:26:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const port = process.env.port || 3000;
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + 'public/index.html');
-// });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'public/index.html');
+});
 
 app.get('/new/*', (req, res) => {
 
